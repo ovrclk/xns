b6fb93a603fb1a35a4fd9deb0e99b2a46ea5d64b
Greg Osuri <gosuri@gmail.com>
2018-08-13 09:19:07 -0700

    handler: resolve root and nameserver domains

--- handler/handler.go
+++ handler/handler.go
@@ -12,22 +12,35 @@ import (
 )
 
 type Handler struct {
-	RootDomain string
+	RootDomain, PublicIP string
+	Nameservers          []string
 
 	dotRegexp, dashRegexp *regexp.Regexp
 }
 
-func New(rootDomain string) *Handler {
+func New(rootDomain, publicIP string, nameServers []string) *Handler {
 	dotted, _ := regexp.Compile(fmt.Sprintf(`(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b).%s.`, rootDomain))
 	dashed, _ := regexp.Compile(fmt.Sprintf(`(\b\d{1,3}\-\d{1,3}\-\d{1,3}\-\d{1,3}\b).%s.`, rootDomain))
 	return &Handler{
-		RootDomain: rootDomain,
-		dotRegexp:  dotted,
-		dashRegexp: dashed,
+		RootDomain:  rootDomain,
+		PublicIP:    publicIP,
+		Nameservers: nameServers,
+		dotRegexp:   dotted,
+		dashRegexp:  dashed,
 	}
 }
 
 func (this *Handler) Resolve(name string) string {
+	// resolve to public ip if root domain
+	if name == this.RootDomain+"." {
+		return this.PublicIP
+	}
+	// resolve to public ip if ns domain
+	for _, ns := range this.Nameservers {
+		if name == ns+"." {
+			return this.PublicIP
+		}
+	}
 	if s := this.dotRegexp.FindStringSubmatch(name); len(s) > 0 {
 		return s[len(s)-1]
 	}
@@ -39,6 +52,7 @@ func (this *Handler) Resolve(name string) string {
 
 func (this *Handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	start := time.Now()
+	log.Printf("[%s] QUESTION: %s", w.RemoteAddr(), r.Question[0].String())
 	msg := dns.Msg{}
 	msg.SetReply(r)
 	switch r.Question[0].Qtype {
@@ -52,7 +66,7 @@ func (this *Handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 				A:   net.ParseIP(address),
 			})
 		}
-		log.Printf("[%s] QUESTION: %s ANSWER: %s. elapsed: %s", w.RemoteAddr(), domain, address, time.Since(start))
+		log.Printf("[%s] DOMAIN: %s ANSWER: %s. elapsed: %s", w.RemoteAddr(), domain, address, time.Since(start))
 	}
 	w.WriteMsg(&msg)
 }
--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var addr, root string
+var addr, root, ip string
+var ns []string
 var cmd = &cobra.Command{
 	Use:   "xns",
 	Short: "xns is a simple and fast wildcard DNS service",
@@ -19,8 +20,10 @@ var cmd = &cobra.Command{
 }
 
 func init() {
+	cmd.PersistentFlags().StringVarP(&ip, "ip", "i", "127.0.0.1", "public IP to resolve root domain and NS records")
 	cmd.PersistentFlags().StringVarP(&addr, "bind", "b", "0.0.0.0:53", "address to bind (udp)")
 	cmd.PersistentFlags().StringVarP(&root, "root", "r", "aksh.io", "root domain (example: aksh.io)")
+	cmd.PersistentFlags().StringSliceVarP(&ns, "ns", "n", []string{"ns1.aksh.io", "ns2.aksh.io"}, "ns servers")
 }
 
 func main() {
@@ -34,7 +37,7 @@ func doStart(addr, root string) {
 	log.Println("starting DNS server, binding to:", addr)
 	log.Println("resolving wildcard addresses for domain:", root)
 	srv := &dns.Server{Addr: addr, Net: "udp"}
-	srv.Handler = handler.New(root)
+	srv.Handler = handler.New(root, ip, ns)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to set udp listener %s\n", err.Error())
 	}
